5c65bb3deca13478b78655803c7f0fb3fc867066
Jamshed Hussain <jamshedhussainjava123@gmail.com>
2026-09-11 21:55:53 +0530

    git commit -m "ECM-4: Implement user registration API"

---
+++ user-service/src/main/java/com/ecommerce/user_service/service/UserService.java
@@ -0,0 +1,31 @@
+package com.ecommerce.user_service.service;
+
+import com.ecommerce.user_service.entity.User;
+import com.ecommerce.user_service.exception.UserAlreadyExistsException;
+import com.ecommerce.user_service.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,PasswordEncoder passwordEncoder){
+        this.userRepository=userRepository;
+        this.passwordEncoder=passwordEncoder;
+    }
+
+
+    public User registerUser(User user) {
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsException("Email already registered");
+        }
+        String hashPassword=passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
+        return userRepository.save(user);
+    }
+
+}
