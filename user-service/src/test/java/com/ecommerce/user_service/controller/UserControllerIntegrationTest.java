11580ace494a2c600f4174c8cf7c674e291bec31
Jamshed Hussain <jamshedhussainjava123@gmail.com>
2026-09-17 11:47:26 +0530

    ECM-4 configure CI integration test database

--- user-service/src/test/java/com/ecommerce/user_service/controller/UserControllerIntegrationTest.java
+++ user-service/src/test/java/com/ecommerce/user_service/controller/UserControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Tag("integration")
 @SpringBootTest
+@ActiveProfiles("ci")
 @AutoConfigureMockMvc
 public class UserControllerIntegrationTest {
 
